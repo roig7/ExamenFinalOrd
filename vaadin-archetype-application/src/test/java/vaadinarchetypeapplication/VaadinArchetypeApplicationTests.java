3366836f4d40cd7247a7c0368a5291cdac124894
roig7 <roig.alberto@hotmail.com>
2021-01-29 18:46:06 +0100

    Creacion del proyecto vaadin

---
+++ vaadin-archetype-application/src/test/java/vaadinarchetypeapplication/VaadinArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package vaadinarchetypeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
